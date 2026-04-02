47aa47265a88b2e6eb3e161cf5e181d235ce6c6b
Anamika <98573911+ana0404@users.noreply.github.com>
2026-04-02 15:30:03 -0700

    Time: 27 ms (93.77%) | Memory: 19.6 MB (62.05%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,17 +1,13 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def is_palindrome(left: int,right: int) -> bool:
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+        
         left = 0 
         right = len(s)-1
         while left < right:
             if s[left] != s[right]:
-                return is_palindrome(left+1,right) or is_palindrome(left, right-1)
+                skipl , skipr = s[left+1 : right+1] , s[left:right]
+                return (skipl == skipl[::-1] or skipr == skipr[::-1])
+                
             left += 1
             right -= 1
         return True
